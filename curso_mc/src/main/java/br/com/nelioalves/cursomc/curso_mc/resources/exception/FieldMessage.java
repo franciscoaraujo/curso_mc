dc08ae6184c939983a1b7a8b3370a4e8de7feb0e
foaj <foaj@BRPC003901.gft.com>
2019-12-16 19:09:14 -0200

    PUT, DELETE e GET para Cliente

---
+++ curso_mc/src/main/java/br/com/nelioalves/cursomc/curso_mc/resources/exception/FieldMessage.java
@@ -0,0 +1,35 @@
+package br.com.nelioalves.cursomc.curso_mc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String fielName;
+	private String message;
+	
+	public FieldMessage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldMessage(String fielName, String message) {
+		super();
+		this.fielName = fielName;
+		this.message = message;
+	}
+
+	public String getFielName() {
+		return fielName;
+	}
+
+	
+	public String getMessage() {
+		return message;
+	}
+
+	
+	
+}
