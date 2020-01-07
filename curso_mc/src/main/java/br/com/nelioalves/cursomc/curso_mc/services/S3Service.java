64d9b55fe45f9d1899b7104d952d934e241d3b4b
foaj <foaj@BRPC003901.gft.com>
2020-01-07 18:14:55 -0200

    Tratando exceções adequadamente

--- curso_mc/src/main/java/br/com/nelioalves/cursomc/curso_mc/services/S3Service.java
+++ curso_mc/src/main/java/br/com/nelioalves/cursomc/curso_mc/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.nelioalves.cursomc.curso_mc.services.exception.FileException;
+
 @Service
 public class S3Service {
 
@@ -35,7 +37,7 @@ public class S3Service {
 
 		} catch (IOException e) {
 
-			throw new RuntimeException("Erro de IO " + e.getMessage());
+			throw new FileException("Erro de IO " + e.getMessage());
 		}
 
 	}
@@ -51,7 +53,7 @@ public class S3Service {
 			return s3Client.getUrl(bucketName, fileName).toURI();
 
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter a Url para Uri");
+			throw new FileException("Erro ao converter a Url para Uri");
 		}
 	}
 
